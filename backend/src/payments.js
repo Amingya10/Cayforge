85c8b01a513e0785d7f32a83b1433f575cb15b25
Amingya10 <amingya.aa@gmail.com>
2026-04-10 21:27:30 +0100

    Delete backend/src/payments.js

--- backend/src/payments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/status', async (req, res) => {
-  res.json({ plan: 'FREE' });
-});
-
-module.exports = router;

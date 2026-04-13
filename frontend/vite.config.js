52ab2401f7ec74e86efb5d2147c50fd803e0f17e
Amingya10 <amingya.aa@gmail.com>
2026-04-13 07:09:41 +0100

    Add multiple entry points for Vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tools: resolve(__dirname, 'tools.html')
+      }
+    }
+  },
   server: {
     port: 5173
   }
